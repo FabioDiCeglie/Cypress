d4bb616229d3b83e67ed2015ea00c252d9ab1d2b
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-05 10:43:44 +0200

    checking every second of the timer

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,6 +1,9 @@
 describe("Timer", () => {
   it("shows 10 seconds", () => {
     cy.visit("/");
-    cy.contains(".status__time", "00:10", { timeout: 11_000 });
+    // cy.contains(".status__time", "00:10", { timeout: 11_000 });
+    for (let k = 0; k < 10; k++) {
+      cy.contains(".status__time", `00:0${k}`);
+    }
   });
 });
