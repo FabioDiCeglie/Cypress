88f608c2c9c4ce1edbfcd236065816e87e43da51
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-06 11:16:00 +0200

    inserting cypress clock in the timer fr check the timer shows

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,3 +1,4 @@
+/// <reference types="cypress"
 describe("Timer", () => {
   it("shows 10 seconds", () => {
     cy.visit("/");
@@ -5,5 +6,19 @@ describe("Timer", () => {
     for (let k = 0; k < 10; k++) {
       cy.contains(".status__time", `00:0${k}`);
     }
+  });
+
+  // confirm the timer shows "00:30" after 30 seconds
+  // confirm the timer shows "11:40" after 700 seconds
+  it("shows minutes and seconds since the game starte", () => {
+    cy.clock();
+    cy.visit("/");
+    cy.contains(".status__time", "00:00");
+    cy.tick(30_000);
+    cy.contains(".status__time", "00:30");
+    cy.tick(30_000);
+    cy.contains(".status__time", "01:00");
+    cy.tick(640_000);
+    cy.contains(".status__time", "11:40");
   });
 });
