c26bf5442d11669016e01620c3fbeff51010eaf1
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-10 19:48:45 +0200

    testing statusSection component with cypress

---
+++ src/components/layout/StatusSection.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+import React from "react";
+import "../../App.css";
+import { StatusSection } from "./StatusSection";
+
+describe("StatusSection", () => {
+  it("changes modes", () => {
+    cy.mount(
+      <div className="innercontainer" style={{ marginTop: "30px" }}>
+        <StatusSection />
+      </div>
+    );
+
+    cy.get(".status__action-mistakes-mode input").should("not.be.checked");
+    cy.get(".status__action-mistakes-mode").click();
+    cy.get(".status__action-mistakes-mode input").should("be.checked");
+
+    cy.get(".status__action-fast-mode input").should("not.be.checked");
+    cy.get(".status__action-fast-mode").click();
+    cy.get(".status__action-fast-mode input")
+      .should("be.checked")
+      .wait(1000, { log: false });
+
+    cy.screenshot("StatusSection - 1");
+    cy.viewport(290, 500);
+    cy.screenshot("StatusSection - 2");
+  });
+});
