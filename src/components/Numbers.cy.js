5cf50bfb8a46911069a3a537e9f595807742d232
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-10 19:23:22 +0200

    add onClick at component level

--- src/components/Numbers.cy.js
+++ src/components/Numbers.cy.js
@@ -5,7 +5,6 @@ import "../App.css";
 
 describe("Numbers", { viewportHeight: 1000, viewportWidth: 1000 }, () => {
   it("shows numbers", () => {
-    // @ts-ignore
     cy.mount(
       <div className="innerContainer">
         <section className="status">
@@ -13,5 +12,17 @@ describe("Numbers", { viewportHeight: 1000, viewportWidth: 1000 }, () => {
         </section>
       </div>
     );
+    cy.get(".status__number").should("have.length", 9);
+  });
+  it("shows numbers", () => {
+    cy.mount(
+      <div className="innerContainer">
+        <section className="status">
+          <Numbers onClickNumber={cy.stub().as("click")} />
+        </section>
+      </div>
+    );
+    cy.contains(".status__number", "1").click();
+    cy.get("@click").should("have.been.calledOnce", "1");
   });
 });
